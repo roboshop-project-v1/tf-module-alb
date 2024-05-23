3cccad2b77c75e37a1758ba8f9cd796cb6137d67
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-23 22:55:20 +0530

    okok

--- main.tf
+++ main.tf
@@ -17,13 +17,16 @@ resource "aws_security_group" "lb_sg" {
 }
 
 
-resource "aws_vpc_security_group_ingress_rule" "lb_sg" {
-  security_group_id = aws_security_group.lb_sg.id
-  cidr_ipv4         = var.sg_ingress_cidr
+
+resource "aws_security_group_rule" "lb_sg" {
+  type              = "ingress"
   from_port         = var.sg_port
-  ip_protocol       = "tcp"
   to_port           = var.sg_port
+  protocol          = "tcp"
+  cidr_blocks       = var.sg_ingress_cidr
+  security_group_id = aws_security_group.lb_sg.id
 }
+
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.lb_sg.id
